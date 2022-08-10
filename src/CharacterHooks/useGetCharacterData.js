85784e8306cf86f16e764cdf7ae74bae055d7779
Salvatore Santamaria <sam.santamaria@gmail.com>
2022-08-10 13:04:41 -0700

    Character name and planet is outputted to screen

--- src/CharacterHooks/useGetCharacterData.js
+++ src/CharacterHooks/useGetCharacterData.js
@@ -1,41 +1,67 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 function useGetCharacterData() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [characters, setCharacters] = useState([]);
-  const [planets, setPlanets] = useState([]);
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [people, setPeople] = useState()
+  const [planets, setPlanets] = useState()
+  let planetURL = "https://swapi.dev/api/planets/?page="
+  let peopleURL = "https://swapi.dev/api/people/?page="
+
+  async function paginated_fetch(url, page = 1, previousResponse = []) {
+    return fetch(`${url}&page=${page}`)
+      .then(
+        (response) => response.json()
+        )
+      .then((newResponse) => {
+        const response = [...previousResponse, ...newResponse.results]
+
+        if (newResponse.next) {
+          page++
+          return paginated_fetch(url, page, response)
+        }
+        return response
+      })
+  }
 
   useEffect(() => {
-    fetch("https://swapi.dev/api/people")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setCharacters(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
-  }, []);
+    paginated_fetch(planetURL)
+    .then((r) => {
+      setPlanets([...r])
+    }, (e) => {
+      setError(e)
+    })
+    paginated_fetch(peopleURL)
+    .then((r) => {
+      setPeople([...r])
+    }, (e) => {
+      setError(e)
+    })
+    setIsLoaded(true)
 
-  useEffect(() => {
-    fetch("https://swapi.dev/api/planets")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setPlanets(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
-  }, []);
+    
+  }, [])
+  
+  const newPlanetMap = new Map()
+  const peopleMap = new Map()
 
-  return [error, isLoaded, characters, planets];
+  if (planets) {
+    const planetMap = new Map(Object.entries(planets))
+    for (let [key, value] of planetMap) {
+      newPlanetMap.set(value.url, value.name)
+    }
+  }
+
+  const updatePeople = (people) => {
+    for (let i in people) {
+      const homeplanet = newPlanetMap.get(people[i].homeworld)
+      peopleMap.set(i, { ...people[i], homeplanet })
+    }
+  }
+  updatePeople(people)
+  const charactersWithPlanets = [...peopleMap.entries()]
+
+  return [charactersWithPlanets, error, isLoaded]
 }
 
-export default useGetCharacterData;
+export default useGetCharacterData
--- src/Characters/characters.js
+++ src/Characters/characters.js
@@ -1,22 +1,24 @@
-import useGetCharacterData from "../CharacterHooks/useGetCharacterData";
+import useGetCharacterData from "../CharacterHooks/useGetCharacterData"
 
 export default function Characters() {
-  const [error, isLoaded, characters, planets] = useGetCharacterData();
+  const [charactersWithPlanets, error, isLoaded] = useGetCharacterData()
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div>Error: {error.message}</div>
   } else if (!isLoaded) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   } else {
     return (
       <div>
         <h1>Characters List</h1>
         <ul>
-          {characters.results.map((c, i) => (
-            <li key={i}>{c.name}</li>
+          {charactersWithPlanets.map((char, key) => (
+            <li key={key}>
+              {char[1].name}, {char[1].homeplanet}
+            </li>
           ))}
         </ul>
       </div>
-    );
+    )
   }
 }
